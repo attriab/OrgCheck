7a1f48882d11aa30b45037793b10e89d8a088d2f
Abhishek Attri <abhishek.attri@blueoptima.com>
2019-07-08 19:44:00 +0530

    #3 : Added binary search method and it's test cases, created new PositionalInteger class to keep a track of integer in array after sorting

--- src/com/aattri90/ds/array/Search.java
+++ src/com/aattri90/ds/array/Search.java
@@ -1,37 +1,70 @@
 package com.aattri90.ds.array;
 
+import com.aattri90.model.PositionalInteger;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Search {
 
+  private static final int NOT_FOUND = -1;
   /**
-   * @question // TODO : add the description
-   * @link
    * @param arr input array
    * @param searchElement element to be searched
    * @return index of search element if present, else returns -1
-   *
+   * @question // TODO : add the description
+   * @link
    */
-  public int linearSearch(int[] arr, int searchElement) {
+  int linearSearch(int[] arr, int searchElement) {
 
-    int notFound = -1;
-    int n = arr.length;
-    if (arr == null || n == 0) {
-      return notFound;
+    if (arr == null || arr.length == 0) {
+      return NOT_FOUND;
     }
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < arr.length; i++) {
       if (arr[i] == searchElement) {
         return i;
       }
     }
 
-    return notFound;
+    return NOT_FOUND;
   }
 
-  public static void main(String[] args) {
-    Search search = new Search();
-    int[] arr = new int[] {1, 2, 3, 4, 5};
-    int searchIndex = search.linearSearch(arr, 4);
-    System.out.println(searchIndex);
+
+  /**
+   * @param arr input array
+   * @param searchElement element to be searched
+   * @param start start index for nbinary search
+   * @param end end index for nbinary search
+   * @return index of search element if present, else returns -1
+   * @question // TODO : add the description
+   * @link
+   */
+  private int binarySearch(PositionalInteger[] arr, int searchElement, int start, int end) {
+
+    if (start > end) {
+      return NOT_FOUND;
+    }
+
+    int mid = (start + end) / 2;
+
+    if (searchElement > arr[mid].getValue()) {
+      return binarySearch(arr, searchElement, mid + 1, end);
+    } else if (searchElement < arr[mid].getValue()) {
+      return binarySearch(arr, searchElement, 0, mid - 1);
+    } else {
+      return arr[mid].getPosition();
+    }
+
+  }
+
+  int binarySearch(PositionalInteger[] arr, int searchElement) {
+
+    if (arr == null || arr.length == 0) {
+      return NOT_FOUND;
+    }
+    Arrays.sort(arr);
+    return binarySearch(arr, searchElement, 0, arr.length - 1);
+
   }
 
 }
--- test/com/aattri90/ds/array/SearchTest.java
+++ test/com/aattri90/ds/array/SearchTest.java
@@ -1,7 +1,8 @@
 package com.aattri90.ds.array;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
 
+import com.aattri90.model.PositionalInteger;
 import org.testng.annotations.Test;
 
 public class SearchTest {
@@ -27,5 +28,34 @@ public class SearchTest {
     assertEquals(search.linearSearch(arr, searchElement), expectedSearchIndex);
 
   }
+
+  @Test
+  public void testBinarySearch1() {
+
+    PositionalInteger[] arr = new PositionalInteger[]{
+        new PositionalInteger(0, 0), new PositionalInteger(1, -4),
+        new PositionalInteger(2, 12), new PositionalInteger(3, 10),
+        new PositionalInteger(4, 22), new PositionalInteger(5, -100),
+        new PositionalInteger(6, 21)};
+    int searchElement = -100;
+    int expectedSearchIndex = 5;
+    assertEquals(search.binarySearch(arr, searchElement), expectedSearchIndex);
+
+  }
+
+  @Test
+  public void testBinarySearch2() {
+
+    PositionalInteger[] arr = new PositionalInteger[]{
+        new PositionalInteger(0, 0), new PositionalInteger(1, -4),
+        new PositionalInteger(2, 12), new PositionalInteger(3, 10),
+        new PositionalInteger(4, 22), new PositionalInteger(5, -100),
+        new PositionalInteger(6, 21)};    int searchElement = 44;
+    int expectedSearchIndex = -1;
+    assertEquals(search.binarySearch(arr, searchElement), expectedSearchIndex);
+
+  }
+
+
 
 }
